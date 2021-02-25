dc524a765ce8202eeb1b95ef8743fdda29f24c40
Tobias Dossinger <tobias.dossinger@googlemail.com>
2021-02-25 23:50:50 +0100

    updates config.js

--- config/config.js
+++ config/config.js
@@ -10,26 +10,26 @@
 
 var config = {
 	address: "localhost", // Address to listen on, can be:
-	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-	                      // - another specific IPv4/6 to listen on a specific interface
-	                      // - "", "0.0.0.0", "::" to listen on any interface
-	                      // Default, when address config is left out, is "localhost"
+	// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+	// - another specific IPv4/6 to listen on a specific interface
+	// - "", "0.0.0.0", "::" to listen on any interface
+	// Default, when address config is left out, is "localhost"
 	port: 8080,
 	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
-	                                                       // or add a specific IPv4 of 192.168.1.5 :
-	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-	                                                       // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+	// or add a specific IPv4 of 192.168.1.5 :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+	// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
 	language: "en",
 	timeFormat: 24,
 	units: "metric",
 	// serverOnly:  true/false/"local" ,
-			     // local for armv6l processors, default 
-			     //   starts serveronly and then starts chrome browser
-			     // false, default for all  NON-armv6l devices
-			     // true, force serveronly mode, because you want to.. no UI on this device
-	
+	// local for armv6l processors, default 
+	//   starts serveronly and then starts chrome browser
+	// false, default for all  NON-armv6l devices
+	// true, force serveronly mode, because you want to.. no UI on this device
+
 	modules: [
 		{
 			module: "alert",
@@ -45,10 +45,10 @@ var config = {
 		{
 			module: "currentweather",
 			position: "top_right",
-			
+
 			config: {
-				location: "Darmstadt",
-				locationID: "2938913",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+				location: "Hamburg",
+				locationID: "2911298",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
 				appid: "77e569d712c6c4b99d128095f614f9b2"
 			}
 		},
@@ -56,17 +56,17 @@ var config = {
 			module: "weatherforecast",
 			position: "top_right",
 			header: "Weather Forecast",
-			
+
 			config: {
-				location: "Darmstadt",
-				locationID: "2938913",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+				location: "Hamburg",
+				locationID: "2911298",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
 				appid: "77e569d712c6c4b99d128095f614f9b2"
 			}
 		},
 		{
 			module: "newsfeed",
 			position: "bottom_bar",
-			
+
 			config: {
 				feeds: [
 					{
@@ -76,11 +76,7 @@ var config = {
 					{
 						title: "Robert Koch-Institut",
 						url: "https://www.rki.de/SiteGlobals/Functions/RSSFeed/RSSGenerator_nCoV.xml?nn=2375210",
-				},
-					{
-						title: "IF-Webseite Activity",
-						url: "https://git.imd.rocks/max.studanski/if-website.atom?feed_token=tk3GkpFxy7iwrDo1yDyh",
-				}
+					}
 				],
 				showSourceTitle: true,
 				showPublishDate: true,
@@ -88,58 +84,58 @@ var config = {
 				broadcastNewsUpdates: true
 			}
 		},
-	  	//~ {
-		    //~ module: 'MMM-COVID19',
-		    //~ position: "bottom_center",
-		    
-		    //~ config: {
-		      //~ header:"",
-		      //~ updateInterval: 300000,
-		      //~ infoRowClass: "small",
-		      //~ worldStats: true,
-		      //~ countries: [ "Germany", "China", "Italy", "Spain", "USA" ],
-		      //~ headerRowClass: "medium",
-		      //~ rapidapiKey : "4470123edemsh2ad735320587eb8p1c455ejsne1291c34a6ea" // this is an example, do not try to use it for real
-		    //~ }
-	  	//~ },
+		//~ {
+		//~ module: 'MMM-COVID19',
+		//~ position: "bottom_center",
+
+		//~ config: {
+		//~ header:"",
+		//~ updateInterval: 300000,
+		//~ infoRowClass: "small",
+		//~ worldStats: true,
+		//~ countries: [ "Germany", "China", "Italy", "Spain", "USA" ],
+		//~ headerRowClass: "medium",
+		//~ rapidapiKey : "4470123edemsh2ad735320587eb8p1c455ejsne1291c34a6ea" // this is an example, do not try to use it for real
+		//~ }
+		//~ },
 		{
-		    module: "MMM-SpaceX",
-		    position: "middle_center",
-		    
-		    config: {
-		        records: 4,
-		        modus: "upcoming",
-		        showExtraInfo: false,
-			showColumnHeader: true,
-		    }
-		},
-		{
-		module: 'MMM-iFrame',
-		position: 'bottom_center',	// This can be any of the regions.
-		config: {
-			// See 'Configuration options' for more information.
-				url: ["https://player.twitch.tv/?channel=montanablack88&muted=true&parent=twitch.tv"],  // as many URLs you want or you can just ["ENTER IN URL"] if single URL.
-				updateInterval: 0.5 * 60 * 1000, // rotate URLs every 30 seconds
-				width: "1280", // width of iframe
-				height: "720", // height of iframe
-				frameWidth: "400" // width of embedded iframe, height is beeing calculated by aspect ratio of iframe
+			module: "MMM-SpaceX",
+			position: "middle_center",
+
+			config: {
+				records: 4,
+				modus: "upcoming",
+				showExtraInfo: false,
+				showColumnHeader: true,
 			}
 		},
+		// {
+		// 	module: 'MMM-iFrame',
+		// 	position: 'bottom_center',	// This can be any of the regions.
+		// 	config: {
+		// 		// See 'Configuration options' for more information.
+		// 		url: ["https://player.twitch.tv/?channel=montanablack88&muted=true&parent=twitch.tv"],  // as many URLs you want or you can just ["ENTER IN URL"] if single URL.
+		// 		updateInterval: 0.5 * 60 * 1000, // rotate URLs every 30 seconds
+		// 		width: "1280", // width of iframe
+		// 		height: "720", // height of iframe
+		// 		frameWidth: "400" // width of embedded iframe, height is beeing calculated by aspect ratio of iframe
+		// 	}
+		// },
 		{
-		  module: "MMM-NowPlayingOnSpotify",
-		  position: "top_left",
+			module: "MMM-NowPlayingOnSpotify",
+			position: "top_left",
 
-		  config: {
-		    clientID: "d97133acc15342cd8e3b86c1f55e0cb2",
-		    clientSecret: "88d979d59dde4793995c566408c74468",
-		    accessToken: "BQBW3sm-erYDu2x0BSPuOXuLmFrz81pSQ-HR1Y8_zZ0RYXG75EPFJ1UKMbLOZ4ip94TwG9cLd4OppzS4R578WyjMmPwfUJyAtnj9MmgkewFROcspPdsZwwvWAXHrQfjDN7HlhOb2XRFOl75lvYuk",
-		    refreshToken: "AQA_JBzJH7CbbcHDlcB_-n5vk2r4IL_XjGODSVNIH92q26R706fgFyVaZOflDZCRjTZpiZzVeAqTAHd9xtWYGFf6r68aWMwP1zJT5DyYsKDL62HjUWZoowQKvZGQIiWlvLg"
-		  }
+			config: {
+				clientID: "d97133acc15342cd8e3b86c1f55e0cb2",
+				clientSecret: "88d979d59dde4793995c566408c74468",
+				accessToken: "BQBW3sm-erYDu2x0BSPuOXuLmFrz81pSQ-HR1Y8_zZ0RYXG75EPFJ1UKMbLOZ4ip94TwG9cLd4OppzS4R578WyjMmPwfUJyAtnj9MmgkewFROcspPdsZwwvWAXHrQfjDN7HlhOb2XRFOl75lvYuk",
+				refreshToken: "AQA_JBzJH7CbbcHDlcB_-n5vk2r4IL_XjGODSVNIH92q26R706fgFyVaZOflDZCRjTZpiZzVeAqTAHd9xtWYGFf6r68aWMwP1zJT5DyYsKDL62HjUWZoowQKvZGQIiWlvLg"
+			}
 		}
-	
+
 	]
 
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== "undefined") {module.exports = config;}
+if (typeof module !== "undefined") { module.exports = config; }
